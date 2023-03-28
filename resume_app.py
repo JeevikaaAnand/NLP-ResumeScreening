c43224d01fb20312a435b108a9f90bde52ee61fd
Jeevikaa Anand <Jeevikaaanand@gmail.com>
2023-03-28 10:42:26 +0530

    Update resume_app.py

--- resume_app.py
+++ resume_app.py
@@ -78,8 +78,9 @@ def main():
         file_details = {"filename":Skills.name,
                         "filetype":Skills.type,"filesize":Skills.size}
         raw_text = str(Skills.read(), encoding = "utf-8")
-        result = prediction(raw_text)
-    st.write('The output is {}'.format(result))
+        
+    result = prediction(raw_text)
+    st.success('The output is {}'.format(result))
 
 if __name__=='__main__':
     main()
